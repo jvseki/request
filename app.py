92310ae9f6307c7762353384029d59a7e3ae67df
jvseki <jv.s.m2006@hotmail.com>
2025-10-23 01:20:32 -0300

    media

--- app.py
+++ app.py
@@ -36,13 +36,13 @@ def status():
 @app.route("/multiplicacao")
 def multiplicacao():
     fator1 = 5
-    fator1 = int(fator1)
     fator2 = request.args.get('fator2')
+    if not fator2:
+        return "Fator2 não fornecido."
+    
     fator2 = int(fator2)
-    if not int(fator2):
-         return "O fator1 não foi fornecido."
-    multiplicacao =  int(fator1 * fator2)
-    return f"A multiplicação de {fator1} e {fator2} é {multiplicacao}."
+    resultado = fator1 * fator2
+    return f"O produto de {fator1} e {fator2} é {resultado}."
  
 
 @app.route("/compra")
@@ -54,6 +54,79 @@ def compra():
         return "Item ou quantidade não fornecidos."
     quantidade = int(quantidade)
     return f"Você comprou {quantidade} unidade(s) de {item}."
+
+
+@app.route("/classificar")
+def classificar():
+    nota = request.args.get('nota')
+    if nota is None:
+        return "Nota não fornecida."
+    nota = float(nota)
+    if nota >= 7.0:
+        return "Aluno com bom desempenho!!"
+    elif 5.0 <= nota < 7.0:
+        return "Aluno em recuperação."
+    else:
+        return "Aluno reprovado."
+    
+@app.route('/senha')
+def senha():
+    tamanho = request.args.get('tamanho')
+    if tamanho is None:
+        return "Tamanho não fornecido."
+    tamanho = int(tamanho)
+    if tamanho < 8:
+        return "Senha fraca."
+    elif 8 <= tamanho <= 12:
+        return "Senha média."
+    else:
+        return "Senha forte."
+    
+
+@app.route('/autenticar')
+def autenticar():
+    user = request.args.get('user')
+    password = request.args.get('password')
+    if user == 'admin' and password == '123':
+        return "Acesso concedido."
+    else:
+        return "Acesso negado."
+    
+@app.route('/mensagem')
+def mensagem():
+    rementente = request.args.get('remetente')
+    destinatario = request.args.get('destinatario')
+    if rementente and destinatario:
+        return f"mensagem de {rementente} para {destinatario} enviada com sucesso."
+    else:
+        return "Remetente ou destinatário não fornecidos."
+   
+
+
+@app.route('/api/versao')
+def versao():
+    versao = 3.12
+    if versao:
+        return f"A versão atual da API é {versao}."
+    else:
+        try:
+            raise ValueError("Versão não disponível.")
+        except ValueError as e:
+            return str(e)
+        
+
+@app.route('/media')
+def media():
+    nota1 = request.args.get('nota1')
+    nota2 = request.args.get('nota2')
+    nota3 = request.args.get('nota3')
+    if nota1 is None or nota2 is None or nota3 is None:
+        return "Todas as três notas devem ser fornecidas."
+    nota1 = float(nota1)
+    nota2 = float(nota2)
+    nota3 = float(nota3)
+    resultado = (nota1 + nota2 + nota3) / 3
+    return f"A média das notas é {resultado}."
 
 
 if __name__ == '__main__':
